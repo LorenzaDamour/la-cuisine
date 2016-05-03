b5e7b471466a1f1dc98274e8aea81b8dee055997
LorenzaDamour <lorenz.974@hotmail.fr>
2016-05-03 15:42:37 +0400

    modif

--- js/main.js
+++ js/main.js
@@ -18,6 +18,30 @@ var coktail1 = "Petit four trocadero";
 var coktail2 = "Mini-bagels briochés";
 var coktail3 = "Canapés créations";
 
+var prixmariage1 = 6;
+var prixmariage2 = 6;
+var prixmariage3 = 6;
+
+var prixbapteme1 = 8;
+var prixbapteme2 = 6;
+var prixbapteme3 = 6;
+
+
+var prixpiecemontee1 = 7;
+var prixpiecemontee2 = 9;
+var prixpiecemontee3 = 2;
+
+var prixanniversaire1 = 6;
+var prixanniversaire2 = 6;
+var prixanniversaire3 = 4;
+
+var prixcoktail1 = 0.7;
+var prixcoktail2 = 1;
+var prixcoktail3 = 0.85;
+
+
+
+
 var descriptionbapteme1 = "Originale ou traditionnelle a la fois, cette pièce montée de choux caramélisés garnis de crème mousseline vanille en forme de berceau est parfaite pour célébrer la naissance, le baptême de votre enfant ou une fête prénatale.Fleurs en pâte à sucre : blanc, rose, bleu, jaune, rouge ou assorties.Plaque nougatine avec inscription personnalisée.";
 var descriptionbapteme2 = "Choupette est un gâteau à base de génoise chocolat et d'une onctueuse crème chocolat OU génoise nature et crème fraise ou vanille, recouvert de pâte de sucre rose et décoré de sujets en sucre.";
 var descriptionbapteme3 = "P'tit Bonhomme est un gâteau à base de génoise et crème chocolat ou génoise nature et crème fraise ou vanille, recouvert de pâte de sucre bleu et décoré de sujets en sucre."
@@ -26,9 +50,9 @@ var descriptionmariage1 = "Fleuri et romantique, ce wedding cake est le dessert 
 var descriptionmariage2 = "Wedding Cake Marylin : pièce montée américaine chic et raffinée pour un mariage en noir et blanc !Thème : ciné, rétro, smoking....Parfums classiques - au choix en Mono ou multi-parfums :Génoise chocolat et crème chocolat OU Génoise nature et crème : vanille, noisette, coco ou fraise OU Compotées de fruits - au choix en Mono ou multi-parfums :Génoise nature,crème vanille et compotée d’abricots OU crème framboisier et compotée de framboises OU crème fraisier et compotée de fraises";
 var descriptionmariage3 = "Wedding Cake Marylin : pièce montée américaine chic et raffinée pour un mariage Jaune !Parfums classiques - au choix en Mono ou multi-parfums :Génoise chocolat et crème chocolat OU Génoise nature et crème : vanille, noisette, coco ou fraiseOUCompotées de fruits - au choix en Mono ou multi-parfums :Génoise nature,crème vanille et compotée d’abricots OU crème framboisier et compotée de framboisesOU crème fraisier et compotée de fraises";
 
-var descriptionannif1 = "Un gâteau de champion, pour l'anniversaire d'un champion ! Offrez à votre enfant le plus beau des anniversaires avec une petite pièce montée Cars et son héros préféré Flash McQueen et impressionnez-le en la personnalisant avec son prénom.Génoise chocolat et crème chocolat OU génoise nature et crème vanille ou fraise.Décors en pâte de sucre blanche et sujets en sucre (non consommables).";
-var descriptionannif2 = "Pour un anniversaire de princesse et très girly ! Offrez à votre fille le plus beau des anniversaires avec cette petite pièce montée sur le thème des princesses (Cendrillon).Génoise chocolat et crème chocolat OU Génoise blanche et crème vanille ou fraise.Décors en pâte de sucre rose et sujets en sucre (non consommables).";
-var descriptionannif3 = "Emerveillez vos enfants avec Le Surprise !Sous sa mousse chocolat se cachent des smarties® colorés, au moment de la découpe du gâteau la surprise est assurée !Mousse chocolat, glaçage chocolat, smarties® et décors en sucre et en chocolat. Plaque en chocolat à personnaliser. ";
+var descriptionanniversaire1 = "Un gâteau de champion, pour l'anniversaire d'un champion ! Offrez à votre enfant le plus beau des anniversaires avec une petite pièce montée Cars et son héros préféré Flash McQueen et impressionnez-le en la personnalisant avec son prénom.Génoise chocolat et crème chocolat OU génoise nature et crème vanille ou fraise.Décors en pâte de sucre blanche et sujets en sucre (non consommables).";
+var descriptionanniversaire2 = "Pour un anniversaire de princesse et très girly ! Offrez à votre fille le plus beau des anniversaires avec cette petite pièce montée sur le thème des princesses (Cendrillon).Génoise chocolat et crème chocolat OU Génoise blanche et crème vanille ou fraise.Décors en pâte de sucre rose et sujets en sucre (non consommables).";
+var descriptionanniversaire3 = "Emerveillez vos enfants avec Le Surprise !Sous sa mousse chocolat se cachent des smarties® colorés, au moment de la découpe du gâteau la surprise est assurée !Mousse chocolat, glaçage chocolat, smarties® et décors en sucre et en chocolat. Plaque en chocolat à personnaliser. ";
 
 var descriptionpiecemontee1 = "Passionnément fruitée et gourmande, cette pièce montée a la crème parfumée framboises, aux framboises et génoise blanche est idéale pour un mariage, des fiançailles ou autre événement festif."
 var descriptionpiecemontee2 = "Moderne et chic, cette pièce montée est également riche en saveurs et textures. Une création gourmande parfaite pour un mariage, des fiançailles, cocktails d'entreprise ou tout autres événements festifs. Craqueline (chocolat), San Marco (vanille) et croquant caramel";
@@ -40,107 +64,304 @@ var descriptioncoktail3 = "Pour un buffet ou un cocktail plein de saveurs, 6 var
 
 
 $(function () {
-$("#register-form").validate();
+  $("#register-form").validate();
 });
 
 $(function () {
-    $("#accordion").accordion();
+  $("#accordion").accordion();
 
 });
 
 $(function () {
-    $("#datepicker").datepicker();
+  $("#datepicker").datepicker();
 });
 
 $(function () {
-    $("#mariage").click(function () {
-        $("#gateau1").attr('src', 'img/' + mariage1 + '1.jpg');
-        $("#gateau2").attr('src', 'img/' + mariage2 + '1.jpg');
-        $("#gateau3").attr('src', 'img/' + mariage3 + '1.jpg');
-    });
+  $("#mariage").click(function () {
+    $("#gateau1").attr('src', 'img/' + mariage1 + '1.jpg');
+    $("#gateau2").attr('src', 'img/' + mariage2 + '1.jpg');
+    $("#gateau3").attr('src', 'img/' + mariage3 + '1.jpg');
+  });
 });
 
 $(function () {
-    $("#annif").click(function () {
-        $("#gateau1").attr('src', 'img/' + annversaire1 + '1.jpg');
-        $("#gateau2").attr('src', 'img/' + annversaire2 + '1.jpg');
-        $("#gateau3").attr('src', 'img/' + annversaire3 + '1.jpg');
-    });
+  $("#annif").click(function () {
+    $("#gateau1").attr('src', 'img/' + annversaire1 + '1.jpg');
+    $("#gateau2").attr('src', 'img/' + annversaire2 + '1.jpg');
+    $("#gateau3").attr('src', 'img/' + annversaire3 + '1.jpg');
+  });
 });
 
 $(function () {
-    $("#bapteme").click(function () {
-        $("#gateau1").attr('src', 'img/' + bapteme1 + '1.jpg');
-        $("#gateau2").attr('src', 'img/' + bapteme2 + '1.jpg');
-        $("#gateau3").attr('src', 'img/' + bapteme3 + '1.jpg');
-    });
+  $("#bapteme").click(function () {
+    $("#gateau1").attr('src', 'img/' + bapteme1 + '1.jpg');
+    $("#gateau2").attr('src', 'img/' + bapteme2 + '1.jpg');
+    $("#gateau3").attr('src', 'img/' + bapteme3 + '1.jpg');
+  });
 });
 
 $(function () {
-    $("#piece").click(function () {
-        $("#gateau1").attr('src', 'img/' + piecemontee1 + '1.jpg');
-        $("#gateau2").attr('src', 'img/' + piecemontee2 + '1.jpg');
-        $("#gateau3").attr('src', 'img/' + piecemontee3 + '1.jpg');
-    });
+  $("#piece").click(function () {
+    $("#gateau1").attr('src', 'img/' + piecemontee1 + '1.jpg');
+    $("#gateau2").attr('src', 'img/' + piecemontee2 + '1.jpg');
+    $("#gateau3").attr('src', 'img/' + piecemontee3 + '1.jpg');
+  });
 });
 
 $(function () {
-    $("#coktail").click(function () {
-        $("#gateau1").attr('src', 'img/' + coktail1 + '1.jpg');
-        $("#gateau2").attr('src', 'img/' + coktail2 + '1.jpg');
-        $("#gateau3").attr('src', 'img/' + coktail3 + '1.jpg');
-    });
+  $("#coktail").click(function () {
+    $("#gateau1").attr('src', 'img/' + coktail1 + '1.jpg');
+    $("#gateau2").attr('src', 'img/' + coktail2 + '1.jpg');
+    $("#gateau3").attr('src', 'img/' + coktail3 + '1.jpg');
+  });
 });
 
 $('#event').on('change', function () {
-    var eventTypeName = $("#event option:selected").val();
+  var eventTypeName = $("#event option:selected").val();
 
-    if (eventTypeName == "mariage") {
-        $('#choix-model').show();
+  if (eventTypeName == "mariage") {
+    $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("Scarlett orchidée");
-        $('#choix-model option').eq(1).text("Black and white");
-        $('#choix-model option').eq(2).text("Jaune passion");
-    }
+    $('#choix-model option').eq(0).text("Scarlett orchidée");
+    $('#choix-model option').eq(1).text("Black and white");
+    $('#choix-model option').eq(2).text("Jaune passion");
+  }
 
-    if (eventTypeName == "bapteme") {
-        $('#choix-model').show();
+  if (eventTypeName == "bapteme") {
+    $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("Berceau");
-        $('#choix-model option').eq(1).text("Choupette");
-        $('#choix-model option').eq(2).text("Pti bonhome");
-    }
+    $('#choix-model option').eq(0).text("Berceau");
+    $('#choix-model option').eq(1).text("Choupette");
+    $('#choix-model option').eq(2).text("Pti bonhome");
+  }
 
-    if (eventTypeName == "anniversaire") {
-        $('#choix-model').show();
+  if (eventTypeName == "anniversaire") {
+    $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("Cars");
-        $('#choix-model option').eq(1).text("Princesse");
-        $('#choix-model option').eq(2).text("Surprise");
-    }
+    $('#choix-model option').eq(0).text("Cars");
+    $('#choix-model option').eq(1).text("Princesse");
+    $('#choix-model option').eq(2).text("Surprise");
+  }
 
-    if (eventTypeName == "piece") {
-        $('#choix-model').show();
+  if (eventTypeName == "piece") {
+    $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("Piece montee rouge passion");
-        $('#choix-model option').eq(1).text("Piece montee la reunionnaise");
-        $('#choix-model option').eq(2).text("Piece montee macarons");
-    }
+    $('#choix-model option').eq(0).text("Piece montee rouge passion");
+    $('#choix-model option').eq(1).text("Piece montee la reunionnaise");
+    $('#choix-model option').eq(2).text("Piece montee macarons");
+  }
 
 
-    if (eventTypeName == "coktail") {
+  if (eventTypeName == "coktail") {
 
-        $('#choix-model option').eq(0).text("Petit four trocadero");
-        $('#choix-model option').eq(1).text("Mini-bagels briochés");
-        $('#choix-model option').eq(2).text("Canapés créations");
-    }
-    $("#choix-model").change();
+    $('#choix-model option').eq(0).text("Petit four trocadero");
+    $('#choix-model option').eq(1).text("Mini-bagels briochés");
+    $('#choix-model option').eq(2).text("Canapés créations");
+  }
+  $("#choix-model").change();
 });
 
 $(function () {
-    $("#choix-model").change(function () {
-        var name = this.options[this.selectedIndex].text;
-        $("#miniature").attr('src', 'img/' + name + '2.jpg');
+  $("#choix-model").change(function () {
+    var name = this.options[this.selectedIndex].text;
+    $("#miniature").attr('src', 'img/' + name + '2.jpg');
 
-    });
+    if (name == mariage1) {
+
+
+      $("#description").text(descriptionmariage1);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == mariage2) {
+
+
+      $("#description").text(descriptionmariage2);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == mariage3) {
+
+
+      $("#description").text(descriptionmariage3);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == bapteme1) {
+
+
+      $("#description").text(descriptionbapteme1);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == bapteme2) {
+
+
+      $("#description").text(descriptionbapteme2);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == bapteme3) {
+
+
+      $("#description").text(descriptionbapteme3);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == annversaire1) {
+
+
+      $("#description").text(descriptionanniversaire1);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == annversaire2) {
+
+
+      $("#description").text(descriptionanniversaire2);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == annversaire3) {
+
+
+      $("#description").text(descriptionanniversaire3);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == piecemontee1) {
+
+
+      $("#description").text(descriptionpiecemontee1);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+    if (name == piecemontee2) {
+
+
+      $("#description").text(descriptionpiecemontee2);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == piecemontee3) {
+
+
+      $("#description").text(descriptionpiecemontee3);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == coktail1) {
+
+
+      $("#description").text(descriptioncoktail1);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == coktail2) {
+
+
+      $("#description").text(descriptioncoktail2);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+
+    if (name == coktail3) {
+
+
+      $("#description").text(descriptioncoktail3);
+      $('#nom').text(name);
+      $('#prix').text("Prix: " + calculprix() + "€" )
+    }
+  });
 });
+
+$(function () {
+  $("#quantity").change(function () {
+    $('#prix').text("Prix: " + calculprix() + "€" )
+  });
+});
+
+function calculprix() {
+  var name = $('#choix-model option:selected').text();
+  var part = $('#quantity').val();
+  var prix = 0;
+  switch (name) {
+    case mariage1:
+    prix = part * prixmariage1;
+    break;
+
+    case mariage2:
+    prix = part * prixmariage2;
+    break;
+
+
+    case mariage3:
+    prix = part * prixmariage3;
+    break;
+
+    case piecemontee1:
+    prix = part * prixpiecemontee1;
+    break;
+
+    case piecemontee2:
+    prix = part * prixpiecemontee2;
+    break;
+
+    case piecemontee3:
+    prix = part * prixpiecemontee3;
+    break;
+
+    case bapteme1:
+    prix = part * prixbapteme1;
+    break;
+
+    case bapteme2:
+    prix = part * prixbapteme2;
+    break;
+
+    case bapteme3:
+    prix = part * prixbapteme3;
+    break;
+
+    case annversaire1:
+    prix = part * prixanniversaire1;
+    break;
+
+    case annversaire2:
+    prix = part * prixanniversaire2;
+    break;
+
+    case annversaire3:
+    prix = part * prixanniversaire3;
+    break;
+
+    case coktail1:
+    prix = part * prixcoktail1;
+    break;
+
+    case coktail2:
+    prix = part * prixcoktail2;
+    break;
+
+    case coktail3:
+    prix = part * prixcoktail3;
+    break;
+
+    default:
+
+  }
+
+  return Math.round(prix);
+}
