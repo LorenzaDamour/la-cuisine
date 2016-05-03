c779310c219f7f81ac7ab6b9bd54de2f116e96b1
LorenzaDamour <lorenz.974@hotmail.fr>
2016-05-03 11:37:37 +0400

    modif

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+var mariage1 = "Scarlett orchidée";
+var mariage2 = "Black and white";
+var mariage3 = "Jaune passion";
+
+var bapteme1= "Berceau";
+var bapteme2= "Choupette";
+var bapteme3= "Pti bonhome";
+
+var annversaire1= "Cars";
+var annversaire2= "Princesse";
+var annversaire3 = "Surprise";
+
+var piecemontee1 = "Piece montee rouge passion";
+var piecemontee2 = "Piece montee la reunionnaise";
+var piecemontee3 ="Piece montee macarons";
+
+var coktail1 = "Petit four trocadero";
+var coktail2 = "Mini-bagels briochés";
+var coktail3 = "Canapés créations";
+
+var descriptionbapteme1 = "Originale ou traditionnelle a la fois, cette pièce montée de choux caramélisés garnis de crème mousseline vanille en forme de berceau est parfaite pour célébrer la naissance, le baptême de votre enfant ou une fête prénatale.Fleurs en pâte à sucre : blanc, rose, bleu, jaune, rouge ou assorties.Plaque nougatine avec inscription personnalisée.";
+var descriptionbapteme2 = "Choupette est un gâteau à base de génoise chocolat et d'une onctueuse crème chocolat OU génoise nature et crème fraise ou vanille, recouvert de pâte de sucre rose et décoré de sujets en sucre.";
+var descriptionbapteme3 = "P'tit Bonhomme est un gâteau à base de génoise et crème chocolat ou génoise nature et crème fraise ou vanille, recouvert de pâte de sucre bleu et décoré de sujets en sucre."
+
+var descriptionmariage1 = "Fleuri et romantique, ce wedding cake est le dessert idéal pour un mariage élégant et printanier !Parfums classiques - au choix en Mono ou multi-parfums :Génoise chocolat et crème chocolatOU Génoise nature et crème : vanille, noisette, coco ou fraise OU Compotées de fruits - au choix en Mono ou multi-parfums :Génoise nature,crème vanille et compotée d’abricots OU crème framboisier et compotée de framboises OU crème fraisier et compotée de fraises";
+var descriptionmariage2 = "Wedding Cake Marylin : pièce montée américaine chic et raffinée pour un mariage en noir et blanc !Thème : ciné, rétro, smoking....Parfums classiques - au choix en Mono ou multi-parfums :Génoise chocolat et crème chocolat OU Génoise nature et crème : vanille, noisette, coco ou fraise OU Compotées de fruits - au choix en Mono ou multi-parfums :Génoise nature,crème vanille et compotée d’abricots OU crème framboisier et compotée de framboises OU crème fraisier et compotée de fraises";
+var descriptionmariage3 = "Wedding Cake Marylin : pièce montée américaine chic et raffinée pour un mariage Jaune !Parfums classiques - au choix en Mono ou multi-parfums :Génoise chocolat et crème chocolat OU Génoise nature et crème : vanille, noisette, coco ou fraiseOUCompotées de fruits - au choix en Mono ou multi-parfums :Génoise nature,crème vanille et compotée d’abricots OU crème framboisier et compotée de framboisesOU crème fraisier et compotée de fraises";
+
+var descriptionannif1 = "Un gâteau de champion, pour l'anniversaire d'un champion ! Offrez à votre enfant le plus beau des anniversaires avec une petite pièce montée Cars et son héros préféré Flash McQueen et impressionnez-le en la personnalisant avec son prénom.Génoise chocolat et crème chocolat OU génoise nature et crème vanille ou fraise.Décors en pâte de sucre blanche et sujets en sucre (non consommables).";
+var descriptionannif2 = "Pour un anniversaire de princesse et très girly ! Offrez à votre fille le plus beau des anniversaires avec cette petite pièce montée sur le thème des princesses (Cendrillon).Génoise chocolat et crème chocolat OU Génoise blanche et crème vanille ou fraise.Décors en pâte de sucre rose et sujets en sucre (non consommables).";
+var descriptionannif3 = "Emerveillez vos enfants avec Le Surprise !Sous sa mousse chocolat se cachent des smarties® colorés, au moment de la découpe du gâteau la surprise est assurée !Mousse chocolat, glaçage chocolat, smarties® et décors en sucre et en chocolat. Plaque en chocolat à personnaliser. ";
+
+var descriptionpiecemontee1 = "Passionnément fruitée et gourmande, cette pièce montée a la crème parfumée framboises, aux framboises et génoise blanche est idéale pour un mariage, des fiançailles ou autre événement festif."
+var descriptionpiecemontee2 = "Moderne et chic, cette pièce montée est également riche en saveurs et textures. Une création gourmande parfaite pour un mariage, des fiançailles, cocktails d'entreprise ou tout autres événements festifs. Craqueline (chocolat), San Marco (vanille) et croquant caramel";
+var descriptionpiecemontee3 = "Tendance et colorée, la pièce montée de Macarons sublimera votre buffet de desserts !Les macarons sont présentés sur un support transparent décoré de ruban en pâte à sucre.6 Parfums de macarons à déguster : ,Chocolat,Vanille,Framboise,Café,Pistache,Citron"
+
+var descriptioncoktail1 = "Découvrez notre plateau de petits fours Trocadéro pour vos buffets et réceptions, 8 recettes incontournables :Blini crème harissa poivrons marinés,Rectangle saumon fumé tzatziki,Club saint-jacques,Sablé parmesan crème de roquette tomate cerise marinée      Carré tajine,Macaron chèvre,Cake thaï crevette sésame,Crumble petit pois courgette";
+var descriptioncoktail2 = "Pratique et convivial, découvrez notre plateau de 24 mini-bagels briochés pour vos buffets et réceptions. Tendances, ils seront également très appréciés pour leur saveurs gourmandes.4 recettes incontournables :- Saumon fumé, fromage frais et ciboulette,- Pastrami, fromage frais persillé,- Chèvre, tomate et pesto,-Poulet rôti, moutarde au moût de raisin."
+var descriptioncoktail3 = "Pour un buffet ou un cocktail plein de saveurs, 6 variétés à déguster :Écrevisse sauce cocktail (pain polaire, sauce béchamel, tomate),Carpaccio parmiggiano (pain noir, parmesan, reggiano, poivron),Scandinave (blini au curry, oeufs de truite),Polaire ( pain polaire, saumon fumé, fromage blanc),Épicurien (pain d'épices, bloc de foie gras de canard, chutney de pêche),L'italien (pain focaccia, tomate séchée, fromage de chèvre) "
+
+
 $(function () {
 $("#register-form").validate();
 });
@@ -13,33 +54,41 @@ $(function () {
 
 $(function () {
     $("#mariage").click(function () {
-        $("#gateau1").attr('src', 'img/mariage1.jpg');
-        $("#gateau2").attr('src', 'img/mariage2.jpg');
-        $("#gateau3").attr('src', 'img/mariage3.jpg');
+        $("#gateau1").attr('src', 'img/' + mariage1 + '1.jpg');
+        $("#gateau2").attr('src', 'img/' + mariage2 + '1.jpg');
+        $("#gateau3").attr('src', 'img/' + mariage3 + '1.jpg');
     });
 });
 
 $(function () {
     $("#annif").click(function () {
-        $("#gateau1").attr('src', 'img/annif1.jpg');
-        $("#gateau2").attr('src', 'img/annif2.jpg');
-        $("#gateau3").attr('src', 'img/annif3.jpg');
+        $("#gateau1").attr('src', 'img/' + annversaire1 + '1.jpg');
+        $("#gateau2").attr('src', 'img/' + annversaire2 + '1.jpg');
+        $("#gateau3").attr('src', 'img/' + annversaire3 + '1.jpg');
     });
 });
 
 $(function () {
     $("#bapteme").click(function () {
-        $("#gateau1").attr('src', 'img/bapteme1.jpg');
-        $("#gateau2").attr('src', 'img/bapteme2.jpg');
-        $("#gateau3").attr('src', 'img/bapteme3.jpg');
+        $("#gateau1").attr('src', 'img/' + bapteme1 + '1.jpg');
+        $("#gateau2").attr('src', 'img/' + bapteme2 + '1.jpg');
+        $("#gateau3").attr('src', 'img/' + bapteme3 + '1.jpg');
     });
 });
 
 $(function () {
-    $("#entreprise").click(function () {
-        $("#gateau1").attr('src', 'img/entreprise1.jpg');
-        $("#gateau2").attr('src', 'img/entreprise2.jpg');
-        $("#gateau3").attr('src', 'img/entreprise3.jpg');
+    $("#piece").click(function () {
+        $("#gateau1").attr('src', 'img/' + piecemontee1 + '1.jpg');
+        $("#gateau2").attr('src', 'img/' + piecemontee2 + '1.jpg');
+        $("#gateau3").attr('src', 'img/' + piecemontee3 + '1.jpg');
+    });
+});
+
+$(function () {
+    $("#coktail").click(function () {
+        $("#gateau1").attr('src', 'img/' + coktail1 + '1.jpg');
+        $("#gateau2").attr('src', 'img/' + coktail2 + '1.jpg');
+        $("#gateau3").attr('src', 'img/' + coktail3 + '1.jpg');
     });
 });
 
@@ -49,48 +98,49 @@ $('#event').on('change', function () {
     if (eventTypeName == "mariage") {
         $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("piece montee");
-        $('#choix-model option').eq(1).text("charlotte");
-        $('#choix-model option').eq(2).text("nougatine");
+        $('#choix-model option').eq(0).text("Scarlett orchidée");
+        $('#choix-model option').eq(1).text("Black and white");
+        $('#choix-model option').eq(2).text("Jaune passion");
     }
 
     if (eventTypeName == "bapteme") {
         $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("arche");
-        $('#choix-model option').eq(1).text("cherubin");
-        $('#choix-model option').eq(2).text("ange");
+        $('#choix-model option').eq(0).text("Berceau");
+        $('#choix-model option').eq(1).text("Choupette");
+        $('#choix-model option').eq(2).text("Pti bonhome");
     }
 
     if (eventTypeName == "anniversaire") {
         $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("pourquoi");
-        $('#choix-model option').eq(1).text("voila");
-        $('#choix-model option').eq(2).text("merci");
+        $('#choix-model option').eq(0).text("Cars");
+        $('#choix-model option').eq(1).text("Princesse");
+        $('#choix-model option').eq(2).text("Surprise");
     }
 
-    if (eventTypeName == "entreprise") {
+    if (eventTypeName == "piece") {
         $('#choix-model').show();
 
-        $('#choix-model option').eq(0).text("google");
-        $('#choix-model option').eq(1).text("atom");
-        $('#choix-model option').eq(2).text("edena");
+        $('#choix-model option').eq(0).text("Piece montee rouge passion");
+        $('#choix-model option').eq(1).text("Piece montee la reunionnaise");
+        $('#choix-model option').eq(2).text("Piece montee macarons");
     }
 
 
-    if (eventTypeName == "special") {
-        $('#choix-model').hide();
-        $('#choix-model option').eq(0).text("");
-        $('#choix-model option').eq(1).text("");
-        $('#choix-model option').eq(2).text("");
+    if (eventTypeName == "coktail") {
+
+        $('#choix-model option').eq(0).text("Petit four trocadero");
+        $('#choix-model option').eq(1).text("Mini-bagels briochés");
+        $('#choix-model option').eq(2).text("Canapés créations");
     }
+    $("#choix-model").change();
 });
 
 $(function () {
     $("#choix-model").change(function () {
         var name = this.options[this.selectedIndex].text;
-        $("#miniature").attr('src', 'img/' + name + '.jpg');
-        alert(name);
+        $("#miniature").attr('src', 'img/' + name + '2.jpg');
+
     });
 });
